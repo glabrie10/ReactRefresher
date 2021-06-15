bc1821eec8ad63962391e518702c8d0a5d7229a2
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2021-06-15 15:37:12 -0600

    Deleted helloworld repository, set up index.html for react with hello.js file above index.jx inside of body.  Created app class in index.js, passed props to and from to hello.js, in hello.js created hello class that rendered hi (props) from (props)

--- client/src/components/index.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable import/extensions */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import App from './App.jsx';
-
-// eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('reactrefresher'));
