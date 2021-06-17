29e721e37457c62f323506c486857ff29cc31cf4
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2021-06-16 18:03:32 -0600

    Initial State:  added a Game file with a Game component with a constuctor with state.  Rendered State in Game component, imported Game Component to app.js.  Rendered Game from here

---
+++ reactState/learn-state/src/Game.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-direct-mutation-state */
+import React, { Component } from 'react';
+
+class Game extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      score: 0,
+      gameOver: false
+    }
+  }
+  render(){
+    return (
+      <div>
+        <h1>your score is: {this.state.score}</h1>
+    </div>
+    )
+  }
+}
+
+export default Game;
