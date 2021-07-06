1a7d6a9c8f7d2bb7834ecbac62579300316f0459
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2021-07-06 10:59:51 -0600

    Created Event folder, created Event component that displays a message when mouse hovers over text, imported into app and rendered

---
+++ ReactEvents/eventsreact/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Event from './Event'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          React Events
+          < Event />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
