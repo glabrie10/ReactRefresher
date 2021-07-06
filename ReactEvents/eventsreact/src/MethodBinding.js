07bdcf4e776e44a0140a130b6b7ef0d716d642d2
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2021-07-06 11:33:53 -0600

    Inline Binding: Passed messages array as object in props using defaultprops, created helper class that picked a random message from props, to use inline method binding, bound this to onMouseEnter(this is random message from props) inside the onMouseEnter event

---
+++ ReactEvents/eventsreact/src/MethodBinding.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class MethodBind extends Component {
+    constructor(props) {
+      super(props);
+      this.dispenseWisdom = this.dispenseWisdom.bind(this);
+    }
+    static defaultProps = {
+      messages: [
+        "A fool thinks himself to be wise, but a wise man knows himself to be a fool.",
+        "Educating the mind without educating the heart is no education at all.",
+        "Not everything that is faced can be changed, but nothing can be changed until it is faced."
+      ]
+    };
+
+    dispenseWisdom() {
+      console.log("THIS IS...", this);
+      let { messages } = this.props;
+      let rIndex = Math.floor(Math.random() * messages.length);
+      console.log(messages[rIndex]);
+    }
+
+    render() {
+      return (
+        <div className='WiseSquare' onMouseEnter={this.dispenseWisdom.bind(this)}>
+          Inline Binding
+        </div>
+      );
+    }
+  }
+
+
+export default MethodBind
+
